fdff5efff58845d83290d9374611437fcc09a9ad
Sulis Tiyah <90247421+sulistiyah@users.noreply.github.com>
2025-04-13 11:41:09 +0700

    Update login-script.js
    Mengganti komentar sesuai dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,21 +5,62 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/*
+* Membuat variabel loginFormElemet untuk tampilan form
+* @constant (HTMLElement)
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+/*
+* Membuat variabel loginEmailElemet untuk tampilan input Email
+* @constant (HTMLElement)
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+/*
+* Membuat variabel loginPasswordElemet untuk tampilan input password
+* @constant (HTMLElement)
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/*
+* Membuat variabel expectedEmail untuk menyimpan informasi email sementara
+* @constant (string)
+*/
 const expectedEmail = 'admin@dicoding.com';
+
+/*
+* Membuat variabel expectedPassword untuk menyimpan informasi Password sementara
+* @constant (string)
+*/
 const expectedPassword = 'superpassword';
 
+/*Menambahkan Aksi klik pada button*/
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
-
+  
+  /*
+  * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan
+  * @constant (string)
+  */
   const email = inputEmailElement.value;
+
+  /*
+  * Membuat variabel password untuk menyimpan nilai password yang didapatkan saat button ditekan
+  * @constant (string)
+  */
   const password = inputPasswordElement.value;
+
+  /* Comment : Memastikan bahwa nilai email dan password yang sesuai dengan nilai yang tersimpan*/
   if (email == expectedEmail && password == expectedPassword) {
+
+    /* Comment : Jika sesuai maka program akan berpindah ke halaman home*/
     goToHome();
+    
   } else {
+    
+    /*Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah*/
     showPopUp();
+    
   }
 });
