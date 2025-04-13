99f44bad7bb09675aa86f07b1d198c9331167f34
Sulis Tiyah <90247421+sulistiyah@users.noreply.github.com>
2025-04-13 11:56:17 +0700

    Update login-script.js

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,30 +1,30 @@
-/* @module Login-Script */
+/** @module Login-Script */
 
-/*
+/**
 * Membuat variabel loginFormElemet untuk tampilan form
 * @constant (HTMLElement)
 */
 const loginFormElement = document.querySelector('#loginForm');
 
-/*
+/**
 * Membuat variabel loginEmailElemet untuk tampilan input Email
 * @constant (HTMLElement)
 */
 const inputEmailElement = document.querySelector('#inputEmail');
 
-/*
+/**
 * Membuat variabel loginPasswordElemet untuk tampilan input password
 * @constant (HTMLElement)
 */
 const inputPasswordElement = document.querySelector('#inputPassword');
 
-/*
+/**
 * Membuat variabel expectedEmail untuk menyimpan informasi email sementara
 * @constant (string)
 */
 const expectedEmail = 'admin@dicoding.com';
 
-/*
+/**
 * Membuat variabel expectedPassword untuk menyimpan informasi Password sementara
 * @constant (string)
 */
@@ -34,27 +34,27 @@ const expectedPassword = 'superpassword';
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
   
-  /*
+  /**
   * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan
   * @constant (string)
   */
   const email = inputEmailElement.value;
 
-  /*
+  /**
   * Membuat variabel password untuk menyimpan nilai password yang didapatkan saat button ditekan
   * @constant (string)
   */
   const password = inputPasswordElement.value;
 
-  /* Comment : Memastikan bahwa nilai email dan password yang sesuai dengan nilai yang tersimpan*/
+  /** Comment : Memastikan bahwa nilai email dan password yang sesuai dengan nilai yang tersimpan*/
   if (email == expectedEmail && password == expectedPassword) {
 
-    /* Comment : Jika sesuai maka program akan berpindah ke halaman home*/
+    /** Comment : Jika sesuai maka program akan berpindah ke halaman home*/
     goToHome();
     
   } else {
     
-    /*Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah*/
+    /** Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah*/
     showPopUp();
     
   }
